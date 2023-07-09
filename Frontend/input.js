b67c04ee3835bb82f8b4fd6c066ebd3765d512f1
Hyunji <daream2@naver.com>
2023-07-09 23:59:24 +0900

    feat-#7 add input.js

---
+++ Frontend/input.js
@@ -0,0 +1,9 @@
+function fieldsetActive()  {
+    const target = document.getElementById('young');
+    target.disabled = false;
+  }
+
+  function fieldsetDisable()  {
+    const target = document.getElementById('young');
+    target.disabled = true;
+  }
