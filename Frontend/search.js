7f23b353564fd701f7759f5555db6ad13ed9fa28
Hyunji <daream2@naver.com>
2023-07-10 14:30:48 +0900

    feat-#7 add autocomplete

---
+++ Frontend/search.js
@@ -0,0 +1,61 @@
+// 자동 완성 기능 구현
+const dataList = ["hollow knight", "league of legends", "노란색", "검정색"];
+
+const $search = document.querySelector("#search");
+const $autoComplete = document.querySelector(".autocomplete");
+
+let nowIndex = 0;
+
+$search.onkeyup = (event) => {
+  // 검색어
+  const value = $search.value.trim().toLowerCase();
+
+  // 자동완성 필터링
+  const matchDataList = value
+    ? dataList.filter((label) => label.includes(value))
+    : [];
+
+  switch (event.keyCode) {
+    // UP KEY
+    case 38:
+      nowIndex = Math.max(nowIndex - 1, 0);
+      break;
+
+    // DOWN KEY
+    case 40:
+      nowIndex = Math.min(nowIndex + 1, matchDataList.length - 1);
+      break;
+
+    // ENTER KEY
+    case 13:
+      document.querySelector("#search").value = matchDataList[nowIndex] || "";
+
+      // 초기화
+      nowIndex = 0;
+      matchDataList.length = 0;
+      break;
+      
+    // 그외 다시 초기화
+    default:
+      nowIndex = 0;
+      break;
+  }
+
+  // 리스트 보여주기
+  showList(matchDataList, value, nowIndex);
+};
+
+const showList = (data, value, nowIndex) => {
+  // 정규식으로 변환
+  const regex = new RegExp(`(${value})`, "g");
+  
+  $autoComplete.innerHTML = data
+    .map(
+      (label, index) => `
+      <div class='${nowIndex === index ? "active" : ""}'>
+        ${label.replace(regex, "<mark>$1</mark>")}
+      </div>
+    `
+    )
+    .join("");
+};
