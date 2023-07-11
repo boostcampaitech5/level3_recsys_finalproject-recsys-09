5c9be4e3c9985685f42c1a261776253b33465085
Hyunji <daream2@naver.com>
2023-07-11 14:39:28 +0900

    feat-#7 disable submit

--- Frontend/next.js
+++ Frontend/next.js
@@ -7,22 +7,85 @@ const tagInput = document.getElementsByName('tag')
 const gameInput = document.getElementById('search')
 const submit = document.getElementById('submit')
 
-function checkFormValidity() {
-    if (ageInput.value && youngInput.value && platformInput.value && playerInput.value && genreInput.value && tagInput.value && gameInput.value) {
-      submit.disabled = false; // 제출 버튼 활성화
-    } else {
-      submit.disabled = true; // 제출 버튼 비활성화
-    }
+let validate = {
+  "ageInput" : false,
+  "platformInput" : false,
+  "playerInput" : false,
+  "genreInput" : false,
+  "tagInput" : false,
+  "gameInput" : false
 }
 
-submit.disabled = true;
+ageInput.forEach(age => age.addEventListener('input', checkFormValidity));
+youngInput.forEach(young => young.addEventListener('input', checkFormValidity));
+platformInput.forEach(platform => platform.addEventListener('input', checkFormValidity));
+playerInput.forEach(player => player.addEventListener('input', checkFormValidity));
+genreInput.forEach(genre => genre.addEventListener('input', checkFormValidity));
+tagInput.forEach(tag => tag.addEventListener('input', checkFormValidity));
+gameInput.addEventListener('input', checkFormValidity)
 
-ageInput.addEventListener('input', checkFormValidity);
-youngInput.addEventListener('input', checkFormValidity);
-platformInput.addEventListener('input', checkFormValidity);
-playerInput.addEventListener('input', checkFormValidity);
-genreInput.addEventListener('input', checkFormValidity);
-tagInput.addEventListener('input', checkFormValidity);
-gameInput.addEventListener('input', checkFormValidity);
+function checkFormValidity() {
+  checkElement();
+  if (validate["ageInput"] && validate["platformInput"] && validate["playerInput"] && validate["genreInput"] && validate["tagInput"] && validate["gameInput"]) {
+    submit.disabled = false; // 제출 버튼 활성화
+  } else {
+    submit.disabled = true; // 제출 버튼 비활성화
+  }
+}
 
-checkFormValidity();
+function checkElement(){
+  // false로 초기화
+  let youngValue = false
+  let platformValue = false
+  let playerValue = false
+  let genreValue = false
+  let tagValue = false
+  let gameValue = false
+
+  if (ageInput[0].checked) {
+    validate["ageInput"] = true
+  }
+  
+  if (ageInput[1].checked) {
+    youngInput.forEach(function(young){
+      if (young.checked) {
+        youngValue = true
+      }
+    })
+    validate["ageInput"] = youngValue
+  }
+
+  platformInput.forEach(function(platform){
+    if (platform.checked) {
+      platformValue = true
+    }
+  })
+
+  playerInput.forEach(function(player){
+    if (player.checked) {
+      playerValue = true
+    }
+  })
+
+  genreInput.forEach(function(genre){
+      if (genre.checked) {
+        genreValue = true // 하나라도 check 되어있으면 true
+      }
+  })
+  
+  tagInput.forEach(function(tag){
+    if (tag.checked) {
+      tagValue = true
+    }
+  })
+
+  if (gameInput.value) {
+    gameValue = true
+  }
+
+  validate["platformInput"] = platformValue
+  validate["playerInput"] = playerValue
+  validate["genreInput"]= genreValue
+  validate["tagInput"] = tagValue
+  validate["gameInput"]= gameValue
+}
