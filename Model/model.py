125e78267060d9c45a7e2552bcdf739abb66879d
NongShiN <hcw1027@gmail.com>
2023-07-12 08:23:03 +0000

    feat-#22 add filter function

--- Model/model.py
+++ Model/model.py
@@ -1,37 +1,53 @@
 # model
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from MetaCriticScraper import MetaCriticScraper
 # data
 import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import os
+from filters import filter
 
 class ContentBaseModel():
-    def __init__(self, user_games_names):
-        self.user_games_names = user_games_names
+    def __init__(self, age, platform, players, major_genre, tag, user_games_names):
+        self.user_games_names = list(user_games_names)
+        self.age = age
+        self.platform = platform
+        self.players = self.players
+        self.major_genre = major_genre
+        self.tag = tag
+
         self.load_game_data()
+        self.preprocess_input()
+        self.filtering_data()
+        
 
     def load_game_data(self):
-        load_dotenv()
-        engine = create_engine(f"postgresql://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_DATABASE']}")
+        engine = create_engine(POSTGRE)
         game_table = "game"
         self.game_table = pd.read_sql_table(table_name=game_table, con=engine)
 
+    def filtering_data(self):
+        self.game_table = filter (self.game_table, self.age, self.platform, self.players, self.major_genre)
+    
     def preprocess_input(self):
+        print("-----------------------------------------------------------------------------")
         self.user_df = pd.DataFrame(columns=['id', 'name', 'genre'])
         for i in self.user_games_names:
             tmp = self.game_table[self.game_table['name'] == i]
             self.user_df = pd.concat([self.user_df, tmp[['id', 'name', 'genre']]], ignore_index=True)
 
     def predict(self):
-        db_df = self.game_table[['name', 'genre']]
+        db_df = self.game_table[['id', 'name', 'genre']]
+
+        print(db_df)
 
         # 기존 게임 데이터프레임과 유저 게임 데이터프레임 통합
         combined_df = pd.concat([db_df, self.user_df], ignore_index=True)
 
-        conbined_df = conbined_df.drop_duplicates(subset=['name'], keep='last')
+        combined_df = combined_df.drop_duplicates(subset=['name'], keep='last')
+
+        # print(combined_df)
 
         # TF-IDF 벡터화 객체 생성
         vectorizer = TfidfVectorizer()
@@ -40,7 +56,7 @@ class ContentBaseModel():
         genre_vectors = vectorizer.fit_transform(combined_df['genre'])
 
         # 추천을 위한 유사도 측정
-        similarities = cosine_similarity(genre_vectors[-len(self.user_games_names):], genre_vectors[:-len(self.user_games_names)])
+        similarities = cosine_similarity(genre_vectors[-2:], genre_vectors[:-2])
         top_similar_indices = similarities.argsort()[0][::-1][:5] #5개 추천
 
         # 추천 게임 목록 생성
@@ -48,7 +64,4 @@ class ContentBaseModel():
 
 
         return recommendations
-    
-    
-
 
