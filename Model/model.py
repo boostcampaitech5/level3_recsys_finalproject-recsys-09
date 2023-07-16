b8e3ef1165b4a00ec1d9f289e8d6bfabdd23d9ff
NongShiN <hcw1027@gmail.com>
2023-07-16 13:31:43 +0000

    style- move file path

--- Model/model.py
+++ Model/model.py
@@ -6,7 +6,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import os
-from filters import filter
+from src.filters import filter
 
 class ContentBaseModel():
     def __init__(self, age, platform, players, major_genre, tag, user_games_names):
