e0f0d65cbc63cc78ce0e22c77e23f401bc218a68
NongShiN <hcw1027@gmail.com>
2023-07-17 13:55:15 +0000

    retactor-#10 apply Recbole inference our case

--- Model/Recbole/inference.py
+++ Model/Recbole/inference.py
@@ -3,11 +3,13 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, default='EASE-Jun-10-2023_14-11-23.pth')
-    parser.add_argument('--topk', type=int, default=10)
+    parser.add_argument('--path', type=str, default='EASE-Jul-17-2023_04-55-08.pth')
+    parser.add_argument('--topk', type=int, default=5)
     parser.add_argument('--save_path', type=str, default='submission.csv')
     
     args = parser.parse_args()
     
     # inference(args.path)
     test_inference(args.path,args.topk,args.save_path)
+    #game_log = [12833, 12864, 11550]
+    #inference(game_log, args.path,args.topk,args.save_path)
