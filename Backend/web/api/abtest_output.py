b064c3beb5c2344ba38a13a5c0b34a3f98d700f3
khj <bbb6254177@gmail.com>
2023-07-18 20:05:26 +0900

    #40 feat: add api for abtest

---
+++ Backend/web/api/abtest_output.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, Request, Depends
+from schemas.response import ABOutputResponse, BaseResponse
+from schemas.request import UserRequest, CBRequest, GPTRequest, FeedbackRequest
+from core.preload import get_template
+from core.output_process import get_response, ab_create_response
+
+output_router = APIRouter(prefix="/output")
+
+@output_router.post("/")
+def output_page(request: Request, user: UserRequest = Depends(UserRequest.as_form)):
+    """
+    user에게 받은 input을 model의 input으로 넘겨주고 추천 game을 받아 output page를 return한다.
+    
+    기능
+    1. 모델 서버로 output 요청하기
+    2. 모델 서버로부터 받은 output과 db를 이용해 사용자에게 제공할 game list 생성 (DB)
+    3. html로 추천 game list를 전달한다.
+    4. 모델 서버로 보낸 input과 모델 서버로부터 받은 output을 logging한다. (선택)
+    """
+    
+    templates =  get_template()
+    
+    # model server로 request 보내기
+    cb_model = get_response(CBRequest, user, 'cb_model')
+    gpt = get_response(GPTRequest, user, 'gpt')
+    
+    # model server response 처리를 통한 추천 game list 생성
+    cb_dic = ab_create_response(cb_model, "id")
+    gpt_dic = ab_create_response(gpt, "name")
+
+    return templates.TemplateResponse("output.html", ABOutputResponse(request=request, cb_model=cb_dic, gpt=gpt_dic).__dict__)
+
+
+@output_router.post("/feedback")
+def get_feedback(request: Request, feedback: FeedbackRequest = Depends(FeedbackRequest.as_form)):
+    
+    templates =  get_template()
+    
+    return templates.TemplateResponse("main.html", BaseResponse(request=request).__dict__)
+
