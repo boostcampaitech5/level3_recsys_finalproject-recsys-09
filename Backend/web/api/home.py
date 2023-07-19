e4835a128edfd70b12f1a4bd7212990be2d5982d
khj <bbb6254177@gmail.com>
2023-07-19 19:10:36 +0900

    #40 feat: add cookie setting and get feedback

--- Backend/web/api/home.py
+++ Backend/web/api/home.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter, Request
-from core.preload import get_template
+from fastapi import APIRouter, Request, Depends
+import uuid
 from schemas.response import BaseResponse
+from schemas.request import FeedbackRequest
+from core.preload import get_template
 
 home_router = APIRouter(prefix="/home")
+
+@home_router.post("/")
+def home_page(request: Request,  feedback: FeedbackRequest = Depends(FeedbackRequest.as_form)):
+    
+    templates =  get_template()
+    print(feedback)
+    
+    return templates.TemplateResponse("main.html", BaseResponse(request=request).__dict__)
+
 
 @home_router.get("/")
 def home_page(request: Request):
@@ -12,4 +23,12 @@ def home_page(request: Request):
     
     templates = get_template()
     
-    return templates.TemplateResponse("main.html", BaseResponse(request=request).__dict__)
+    response = templates.TemplateResponse("main.html", BaseResponse(request=request).__dict__)
+    
+    cookie_id = request.cookies.get("id")
+    
+    if not cookie_id:
+        cookie_id = str(uuid.uuid4())
+        response.set_cookie(key="id", value=cookie_id, httponly=True)
+    
+    return response
