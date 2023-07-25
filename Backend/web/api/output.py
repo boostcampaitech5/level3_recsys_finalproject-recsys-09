645c497b8e825d0233122f11da0ab82d0c9c1bf9
khj <bbb6254177@gmail.com>
2023-07-25 22:05:35 +0900

    #65 feat: create id and save user input, model output

--- Backend/web/api/output.py
+++ Backend/web/api/output.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter, Request, Depends
-from schemas.response import OutputResponse, BaseResponse
-from schemas.request import UserRequest, ModelRequest, GPTRequest, FeedbackRequest
+import uuid
+from google.cloud import bigquery
+from schemas.response import OutputResponse
+from schemas.request import UserRequest, ModelRequest, GPTRequest
 from core.preload import get_template
+from core.save_db import save_user_info, save_model_output
 from core.output_process import get_response, create_response
+from database.bigquery import get_bigquery_client
 
 output_router = APIRouter(prefix="/output")
 
 @output_router.post("/")
-def output_page(request: Request, user: UserRequest = Depends(UserRequest.as_form)):
+def output_page(request: Request, user: UserRequest = Depends(UserRequest.as_form), client: bigquery.Client = Depends(get_bigquery_client)):
     """
     user에게 받은 input을 model의 input으로 넘겨주고 추천 game을 받아 output page를 return한다.
     
@@ -18,11 +22,15 @@ def output_page(request: Request, user: UserRequest = Depends(UserRequest.as_for
     4. 모델 서버로 보낸 input과 모델 서버로부터 받은 output을 logging한다. (선택)
     """
     
-    templates =  get_template()
+    templates = get_template()
+
+    id = str(uuid.uuid4())
+    save_user_info(id, user, client)
     
     # model server로 request 보내기
     hb_model = get_response(ModelRequest, user, 'hb_model')
     gpt = get_response(GPTRequest, user, 'gpt')
+    save_model_output(id, hb_model, gpt, client)
     
     # model server response 처리를 통한 추천 game list 생성
     game_dic = create_response(hb_model, gpt, user)
