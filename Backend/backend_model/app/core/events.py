7248b04764faa1cf28c00b9cc7493241a10d6d8b
HeewonKwak <hewo1217@gmail.com>
2023-07-26 11:27:39 +0900

    refactor: remove bigquery #73

--- Backend/backend_model/app/core/events.py
+++ Backend/backend_model/app/core/events.py
@@ -1,46 +1,19 @@
 from fastapi import FastAPI
 from core.config import POSTGRE, MODEL_IP, REDIS_PORT, JSON_PATH
 from direct_redis import DirectRedis
-
-from google.cloud import bigquery
-from google.oauth2 import service_account
-
-def read_bigquery(table_name):
-    # Credentials 객체 생성
-    credentials = service_account.Credentials.from_service_account_file(JSON_PATH)
-
-    # GCP 클라이언트 객체 생성
-    client = bigquery.Client(credentials = credentials, 
-                            project = credentials.project_id)
-
-    # 데이터 조회 쿼리
-    sql = """
-    SELECT
-    *
-    FROM
-    test_game_total.{}
-    """.format(table_name)
-
-    # 데이터 조회 쿼리 실행 결과
-    query_job = client.query(sql)
-
-    # 데이터프레임 변환
-    df = query_job.to_dataframe()
-    if 'id' in df.columns:
-        df['id'] = df['id'].astype('int64')
-    return df
-
-import os
+from sqlalchemy import create_engine
+import pandas as pd
 
 redis_client = DirectRedis(host=MODEL_IP, port=REDIS_PORT)
 
 def redis_use():
-    # read by bigquery
-    game_table = read_bigquery("game")
-    cb_model_table = read_bigquery("cb_model")
-    Ease_table = read_bigquery("ease")
-    cf_table = read_bigquery("cf_model")
-    details_table = read_bigquery("details")
+    # Create a sample DataFrame
+    engine = create_engine(POSTGRE)
+    game_table = pd.read_sql_table(table_name="game", con=engine)
+    cb_model_table = pd.read_sql_table(table_name="cb_model", con=engine)
+    Ease_table = pd.read_sql_table(table_name="Ease", con=engine)
+    cf_table = pd.read_sql_table(table_name="cf_model", con=engine)
+    details_table = pd.read_sql_table(table_name="details", con=engine)
 
     # Store DataFrame data in Redis with a key (e.g., 'df_cache')
     redis_client.set('game', game_table)
