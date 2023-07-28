311561fb4c3dc342ff86d399d69584eccf83335f
khj <bbb6254177@gmail.com>
2023-07-28 12:30:58 +0900

    #87 feat: redirect to home directory

--- Backend/web/app.py
+++ Backend/web/app.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 from api.home import home_router
 from api.input import input_router
 from api.output import output_router
 from core.preload import preload
+from core.config import HOST, PORT
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="./Frontend"), name="static")
 
+@app.get("/")
+def redirect_home():
+    return RedirectResponse(f"http://{HOST}:{PORT}/home")
 
 if __name__ == '__main__':
     app.add_event_handler('startup', preload)
